fc831ac23a0b5833213415bef31bac63bc4770d7
manimaran990 <manimaran990@gmail.com>
2014-06-15 17:56:31 +0530

    first commit

---
+++ puzzle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import re
+
+words = open('words','r')
+length = raw_input('enter word len : ')
+pattern = str(raw_input('enter pattern : '))
+pattern = pattern.replace('?','\w')
+print ''
+for word in words:
+	if len(word)== int(length)+1 and re.search(pattern,word):
+		print word.strip()
+
