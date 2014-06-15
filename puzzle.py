e3a819422b20436fa1cf6d4c4ba73ef34ece8d51
manimaran990 <manimaran990@gmail.com>
2014-06-15 18:25:02 +0530

    added changes

--- puzzle.py
+++ puzzle.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python
 import re
+import sys
 
-words = open('/usr/share/dict/words','r')
-pattern = str(raw_input('enter pattern : '))
+if len(sys.argv[:]) != 2:
+	print "usage: ./puzzle.py 'pattern'"
+	exit(0)
+
+pattern = sys.argv[1]
 length = len(pattern)
 pattern = pattern.replace('?','\w')
+words = open('/usr/share/dict/words','r')
 print ''
 for word in words:
 	if len(word)== int(length)+1 and re.search(pattern,word):
